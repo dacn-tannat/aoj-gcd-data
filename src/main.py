73888adc254a295a54037d2d33ccf3be727c8aab
duyanhhh1811 <duyanhlucas302@gmail.com>
2024-10-12 10:06:42 +0700

    docs: add usage in main.py

--- src/main.py
+++ src/main.py
@@ -8,4 +8,9 @@ the raw data stored in 'raw_data_c.json' when run as the main program.
 from services import *
 
 if __name__ == '__main__':
+    '''
+    Usage:
+        process_data() if data has not been crawled yet
+        process_data('raw_data_c.json') if the json file already exists
+    '''
     data = process_data('raw_data_c.json')
